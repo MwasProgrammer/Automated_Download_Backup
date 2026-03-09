c0da06a34b063947a8d46cf5fe930c98920c0955
MwasProgrammer <gichimupeter07@gmail.com>
2026-03-09 23:18:53 +0300

    Refactored auditor.py script to include Logger module and the Pathlib implementation.

--- modules/auditor.py
+++ modules/auditor.py
@@ -1,23 +1,23 @@
-import os
 import hashlib # Import hashlib for file hashing 
+import  logging
+from pathlib import Path
 
-def scan_downloads_files(source_path, config):
-    forbidden_files = config['safety_measures']['forbidden_extensions'] # List of forbidden file extensions
+logger = logging.getLogger('backup_downloads_logger.auditor')
 
-    all_downloaded_items = os.listdir(source_path) # List all items in the source path
+def scan_downloads_files(source_path: Path, config: dict) -> list[Path]:
+    forbidden_files = config['safety_measures'].get('forbidden_extensions', []) # List of forbidden file extensions
 
-    ready_files = [] # List to store paths of files that are ready for backup
-    for item in all_downloaded_items: 
-        full_path = os.path.join(source_path, item) 
-        if os.path.isfile(full_path): # Check if the item is a file
-            file_extension = os.path.splitext(item)[1].lower() 
-            if file_extension not in forbidden_files: 
-                ready_files.append(full_path) # Add the full path of the file to the list of ready files
+    ready_files = [ # List to store paths of files that are ready for backup
+        item for item in source_path.iterdir() # Returns Path object
+        if item.is_file() and item.suffix.lower() not in forbidden_files
+    ]
+
+    logger.info(f"Scanned {len(ready_files)} in {source_path} folder, ready for backup.")
 
     return ready_files
 
 # Hash a file using SHA256 algorithm to generate a unique identifier for the file content
-def calculate_file_hash(file_path, chunk_size=8192):
+def calculate_file_hash(file_path: Path, chunk_size: int= 8192) -> str  :
     sha256_hash = hashlib.sha256() # Create a SHA256 hash object
     
     try:
@@ -25,21 +25,22 @@ def calculate_file_hash(file_path, chunk_size=8192):
             while chunk := f.read(chunk_size): # Read the file in chunks to handle large files efficiently
                 sha256_hash.update(chunk)
         return sha256_hash.hexdigest() # Return the hexadecimal representation of the hash
-    except Exception as e:
-        print(f"Error calculating hash for {file_path}: {e}")
-        return None
+    except (OSError, IOError) as e:
+        logger.error(f"File error hashing {file_path.name}. {e}")
+        return ""
     
 
 # Check for duplicate files in the downloads folder 
-def duplicate_files_check(ready_files):
+def duplicate_files_check(ready_files: list[Path]) -> list[Path]:
     calculated_file_hashes = {} # Dictionary to store file hashes and their corresponding file paths
     duplicate_files = [] # List to store paths of duplicate files
 
     for file_path in ready_files:
         file_hash = calculate_file_hash(file_path) 
-        if file_hash is None:
-            continue # Skip files that couldn't be hashed
+        if not file_hash.strip():
+            continue 
         if file_hash in calculated_file_hashes:
+            logger.warning(f"Duplicate file {file_path.name} matches {calculated_file_hashes[file_hash].name}")
             duplicate_files.append(file_path)
         else:
             calculated_file_hashes[file_hash] = file_path # Store the file path for the calculated hash
