acc30e6b62c7c61ef4d518992d5b5d8836d5e4dc
MwasProgrammer <gichimupeter07@gmail.com>
2026-03-12 22:31:35 +0300

    Rebuilt git index to update the refactored .gitignore file

--- modules/executor.py
+++ modules/executor.py
@@ -1,7 +1,11 @@
-import os
+from pathlib import Path
 import shutil
+import logging
+from modules.auditor import calculate_file_hash
 
-def check_disk_space(backup_drive):
+logger = logging.getLogger('backup_downloads_logger.executor')
+
+def check_disk_space(backup_drive: Path) -> bool:
     total, used, free = shutil.disk_usage(backup_drive)
     free_gb = free // (2**30)
     # Check if free space is less than 1GB
@@ -9,8 +13,8 @@ def check_disk_space(backup_drive):
         raise OSError(F"Disk storage space critical: Remaining storage space on {backup_drive} is {free_gb}GB!")
     return True
 
-def get_destination_path(file_path, config):
-    file_extension = os.path.splitext(file_path)[1].lower()
+def get_destination_path(file_path: Path, config: dict, backup_root_path: Path) -> Path:
+    file_extension = file_path.suffix.lower()
     categories = config['backup_target'].get('backup_folders', {})
 
     category_folder = "Others"
@@ -19,16 +23,44 @@ def get_destination_path(file_path, config):
         if file_extension in extensions:
             category_folder = category
             break
+
+    return backup_root_path / category_folder / file_path.name 
+
+# Verify Before Delete Logic
+def move_to_backup_drive(source_file: Path, destination_file: Path) -> bool:
+    temp_dest = None
+
+    try:
+        destination_file.parent.mkdir(parents = True, exist_ok = True) # Ensure the destination directory exists
+        temp_dest = destination_file.with_suffix(destination_file.suffix + '.tmp')
+
+        logger.info(f"Stagging: {source_file.name} as {temp_dest.name}")
+
+        shutil.copy2(source_file, temp_dest)
+        logger.info(f"Backed up file {source_file.name} to {temp_dest}")
+
+        # Verify the calculated hash, if they match.
+        source_hash = calculate_file_hash(source_file)
+        temp_hash = calculate_file_hash(temp_dest)
+
+        # Atomic commit or rollback
+        if source_hash and source_hash == temp_hash:
+            temp_dest.rename(destination_file)
+
+            # Delete files after 100% of Backup verification.
+            source_file.unlink()
+            logger.info(f"{source_file.name} verified and backed up.")
+        
+            return True
+        else:
+            raise ValueError(f"Hash mismatch for {source_file.name} - Integrity failure.")
+        
+    except Exception as e:
+        logger.error(f"Verify Before Delete logic failure for {source_file.name} -> {e}")
+        if temp_dest and temp_dest.exists():
+            temp_dest.unlink()
+            logger.warning(f"Rolled back transaction. File {temp_dest.name} is incomplete or corrupt!")
+
+        return False
+
     
-    if config['source_settings'].get('sandbox_mode'):
-        backup_root = config['backup_target']['backup_directory_path'] # Use the specified backup directory path in sandbox mode
-    else:
-        backup_root = os.path.join("D:/", config['backup_target']['backup_directory_name']) 
-
-    return os.path.join(backup_root, category_folder,os.path.basename(file_path))
-
-def move_to_backup_drive(source_file, destination_file):
-    os.makedirs(os.path.dirname(destination_file), exist_ok=True) # Ensure the destination directory exists
-
-    shutil.move(source_file, destination_file)
-    print(f"Backed up file {os.path.basename(source_file)} to {os.path.dirname(destination_file)}")
