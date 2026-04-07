2646bd37beea76873a5c3de8c3b455d42d0c2d06
MwasProgrammer <gichimupeter07@gmail.com>
2026-04-07 06:22:37 +0300

    Refactored exception validation unit test with user, system and environment error categorization.

--- tests/exception_validation_unit_test.py
+++ tests/exception_validation_unit_test.py
@@ -1,15 +1,14 @@
 import json
-import os
 from pathlib import Path
 from main import main, load_config
 from modules.error_exceptions import ConfigurationError, EnvironmentError
 import logging
 from modules.backup_downloads_logger import configure_backup_downloads_logger
 
-test_logger = configure_backup_downloads_logger()
+exception_validation_test_logger = configure_backup_downloads_logger()
 
 def run_exception_validation_test():
-    test_logger.info(f"DRY RUN for exception_validation unit test.")
+    exception_validation_test_logger.info(f"DRY RUN for exception_validation unit test.")
     original_config = ""
     config_path = Path("config.json")
 
@@ -20,15 +19,17 @@ def run_exception_validation_test():
             f.write("{this_is_corrupt_json}")
     
     try:
-        test_logger.info(f"Testing laod configuration with a 'corrupt' version JSON.")
+        exception_validation_test_logger.info(f"Testing laod configuration with a 'corrupt' version JSON.")
         load_config()
 
     except Exception as e:
-        test_logger.info(f"Successfully caught expected error as {e}")
+        exception_validation_test_logger.info(f"Successfully caught expected error as {e}")
     
     finally:
         with open (config_path, "w") as f:
             f.write(original_config)
+
+        exception_validation_test_logger.info(f"config.json has been restored successfully.")
 
         
 
